bf018173f4d754d199b8bcc7d0364e7e40fd8149
mcdave <mcpointsolutions01@gmail.com>
2025-05-09 11:07:15 +0100

    Just added changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ux=al4!gzy9lubale9b_o4$3fef#s2dr9^lr5k175#@b4&p0as'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mcdave.pythonanywhere.com']
 
 
 # Application definition
